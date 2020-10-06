ac69f12acab8bc66107bb16376d588c53232ad34
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-06 09:34:16 +0200

    refactor: remove typo3-filelist dummy component

---
+++ Build/src/dropzone/typo3-dropzone.ts
@@ -0,0 +1,3 @@
+import { Typo3Dropzone } from './Typo3Dropzone';
+
+customElements.define('typo3-dropzone', Typo3Dropzone);
--- Build/src/typo3-dropdown.ts
+++
@@ -1,3 +0,0 @@
-import { Typo3DropDown } from './Typo3DropDown.js';
-
-customElements.define('typo3-dropdown', Typo3DropDown);
