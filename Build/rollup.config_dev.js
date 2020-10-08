0c6110b8feb1386f608ba772708ae7c8855494cf
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-08 10:32:16 +0200

    fix: use rollup to compile test files

--- Build/rollup.config_dev.js
+++ Build/rollup.config_dev.js
@@ -1,32 +1,25 @@
 import commonJs from 'rollup-plugin-commonjs';
-import image from 'rollup-plugin-img';
-import json from 'rollup-plugin-json';
 import litSass from '@ponday/rollup-plugin-lit-sass';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescriptPlugin from 'rollup-plugin-typescript';
-import typescript from 'typescript';
 
 const globby = require('globby');
-const configs = globby.sync('src/*/**/*.ts').map(inputFile => ({
+
+globby.sync('(src|test)/**/*.ts').forEach((inputFile) => console.log(inputFile));
+
+const configs = globby.sync('(src|test)/**/*.ts').map(inputFile => ({
   input: inputFile,
+  inlineDynamicImports: true,
   output: {
-    file: inputFile.replace('src', 'out-tsc/src').replace('.ts', '.js'),
+    file: 'out-tsc/' + inputFile.replace('.ts', '.js'),
     format: 'esm',
-    sourcemap: true
+    sourcemap: false
   },
   plugins: [
-    image(),
-    json({
-      preferConst: true
-    }),
     litSass(),
     nodeResolve(),
     commonJs(),
-    typescriptPlugin({
-      exclude: 'node_modules/**',
-      importHelpers: true,
-      typescript,
-    })
+    typescriptPlugin()
   ],
   treeshake: true,
 }));
