96a5befac1ff81913b73746b1fa51dea86a7f54c
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-08 17:32:04 +0200

    feat: replace splitbutton by dropdown button

---
+++ Build/src/button/typo3-dropdown-button.ts
@@ -0,0 +1,3 @@
+import { Typo3DropdownButton } from './Typo3DropdownButton';
+
+customElements.define('typo3-dropdown-button', Typo3DropdownButton);
--- Build/src/button/typo3-split-button.ts
+++
@@ -1,3 +0,0 @@
-import { Typo3SplitButton } from './Typo3SplitButton';
-
-customElements.define('typo3-split-button', Typo3SplitButton);
