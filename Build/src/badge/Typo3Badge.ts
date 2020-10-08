af0a7c62a3ac929efcfd01e57ac0b771ac60efe4
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-08 17:01:09 +0200

    feat: add color attribute for badges

--- Build/src/badge/Typo3Badge.ts
+++ Build/src/badge/Typo3Badge.ts
@@ -2,8 +2,18 @@ import { css, html, LitElement, property, TemplateResult } from 'lit-element';
 
 import style from './typo3-badge.scss';
 
+export type Color =
+  | 'default'
+  | 'primary'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'danger';
+
 export class Typo3Badge extends LitElement {
   @property({ type: String }) title = '';
+
+  @property({ type: String, reflect: true }) color: Color = 'default';
 
   public static styles = style({ css });
 
