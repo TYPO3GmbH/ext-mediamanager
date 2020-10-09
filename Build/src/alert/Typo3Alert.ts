e40c8eb073d7ae8d9e41e6bf88374250d88ed00d
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-09 20:34:17 +0200

    feat: add Typo3Alert features dismissable

--- Build/src/alert/Typo3Alert.ts
+++ Build/src/alert/Typo3Alert.ts
@@ -1,13 +1,43 @@
-import { css, html, LitElement, property, TemplateResult } from 'lit-element';
+import {
+  css,
+  html,
+  internalProperty,
+  LitElement,
+  property,
+  TemplateResult,
+} from 'lit-element';
 import style from '../alert/typo3-alert.scss';
-import { Color } from '../badge/Typo3Badge';
+
+export type Color = 'default' | 'success' | 'info' | 'warning' | 'danger';
 
 export class Typo3Alert extends LitElement {
   @property({ type: String, reflect: true }) color: Color = 'default';
 
+  @property({ type: Boolean, reflect: true }) dismissable = false;
+
+  @internalProperty() isHidden = false;
+
   public static styles = style({ css });
 
   render(): TemplateResult {
-    return html` <slot></slot> `;
+    return html`
+      ${!this.isHidden
+        ? html` <div id="alert">
+            <div id="message">
+              <slot></slot>
+            </div>
+            ${this.dismissable
+              ? html`<button id="btn-close" @click="${this.close}">×</button>`
+              : ''}
+          </div>`
+        : ''}
+    `;
+  }
+
+  close(): void {
+    if (this.dismissable) {
+      this.isHidden = true;
+      this.dispatchEvent(new CustomEvent('typo3-alert-closed'));
+    }
   }
 }
--- Build/test/typo3-alert.test.ts
+++ Build/test/typo3-alert.test.ts
@@ -1,17 +1,53 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import '../src/alert/typo3-alert.js';
 import { Typo3Alert } from '../src/alert/Typo3Alert';
 
 describe('Typo3Alert', () => {
   let element: Typo3Alert;
   beforeEach(async () => {
-    element = await fixture(html` <typo3-alert title="500"></typo3-alert> `);
+    element = await fixture(html` <typo3-alert>Alert</typo3-alert> `);
   });
 
-  it('displays the `title` attribute', () => {
-    const root = element.shadowRoot ? element.shadowRoot : element;
+  it('displays the slot content', () => {
+    expect(element.textContent).to.contain('Alert');
+  });
 
-    expect(root).to.not.be.null;
-    expect(root.textContent).to.contain('500');
+  it('displays no close button on default', () => {
+    const closeButton = element.shadowRoot!.querySelector('#btn-close')!;
+    expect(closeButton).to.be.null;
+  });
+
+  it('displays a close button when dismissable', async () => {
+    const dismissableAlert = await fixture(
+      html` <typo3-alert dismissable="true">Alert</typo3-alert> `
+    );
+    const closeButton = dismissableAlert.shadowRoot!.querySelector(
+      '#btn-close'
+    );
+    expect(closeButton).not.to.be.null;
+  });
+
+  it('can close alert when dismissable', async () => {
+    const dismissableAlert = (await fixture(
+      html` <typo3-alert dismissable="true">Alert</typo3-alert> `
+    )) as Typo3Alert;
+    const closeButton = dismissableAlert.shadowRoot!.querySelector(
+      '#btn-close'
+    ) as HTMLButtonElement;
+    closeButton.click();
+
+    await elementUpdated(dismissableAlert);
+
+    const alertDiv = dismissableAlert.shadowRoot!.querySelector('#alert')!;
+    expect(alertDiv).to.be.null;
+  });
+
+  it('can not close alert when not dismissable', async () => {
+    element.close();
+
+    await elementUpdated(element);
+
+    const alertDiv = element.shadowRoot!.querySelector('#alert')!;
+    expect(alertDiv).not.to.be.null;
   });
 });
