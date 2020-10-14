db9ca278b5c728c8bb07b15ceea49aea1d276d41
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-14 13:53:42 +0200

    feat: add typo3-selection-button.scss

---
+++ Build/packages/button/src/typo3-selection-button.ts
@@ -0,0 +1,54 @@
+import {
+  css,
+  customElement,
+  html,
+  LitElement,
+  property,
+  query,
+  TemplateResult,
+} from 'lit-element';
+
+import style from './typo3-selection-button.scss';
+import { Typo3Button } from './typo3-button';
+
+@customElement('typo3-selection-button')
+export class Typo3SelectionButton extends LitElement {
+  @property({ type: Number, reflect: true }) count = 0;
+
+  @property({ type: String, reflect: true }) suffix = 'Selected';
+
+  @property({ type: String, reflect: true }) color = 'primary';
+
+  @query('typo3-button') button?: Typo3Button;
+
+  public static styles = style({ css });
+
+  render(): TemplateResult {
+    if (0 === this.count) {
+      return html``;
+    }
+    return html`
+      <div class="button">${this.count} ${this.suffix}</div>
+      <typo3-button
+        color="${this.color}"
+        icon-right
+        @click="${this.clearSelection}"
+        ><svg
+          slot="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+          /></svg
+      ></typo3-button>
+    `;
+  }
+
+  clearSelection(): void {
+    this.count = 0;
+    this.dispatchEvent(new CustomEvent('typo3-selection-clear'));
+  }
+}
---
+++ Build/packages/button/test/typo3-selection-button.test.ts
@@ -0,0 +1,62 @@
+import {
+  elementUpdated,
+  expect,
+  fixture,
+  html,
+  oneEvent,
+} from '@open-wc/testing';
+import '../src/typo3-selection-button.js';
+import { Typo3SelectionButton } from '../src/typo3-selection-button';
+
+describe('Typo3SelectionButton.ts', () => {
+  let element: Typo3SelectionButton;
+  beforeEach(async () => {
+    element = await fixture(html`
+      <typo3-selection-button suffix="TestSelected" count="5"
+        >Hello World</typo3-selection-button
+      >
+    `);
+  });
+
+  it('can create component', () => {
+    expect(element).to.not.be.null;
+  });
+
+  it('renders the current count', () => {
+    const textbutton = element.shadowRoot!.querySelector('.button')!;
+    expect(textbutton.textContent).to.contain('5 TestSelected');
+  });
+
+  it('renders a typo3-button', () => {
+    const clearButton = element.shadowRoot!.querySelector('typo3-button')!;
+    expect(clearButton.textContent).not.to.be.null;
+  });
+
+  it('renders an empty element on count `0`', async () => {
+    element.count = 0;
+    await elementUpdated(element);
+    expect(element.shadowRoot!).to.be.empty;
+  });
+
+  it('fires a `typo3-selection-clear` event on clear ', async () => {
+    const listener = oneEvent(element, 'typo3-selection-clear');
+    const clearButton = element.shadowRoot!.querySelector(
+      'typo3-button'
+    ) as HTMLButtonElement;
+    clearButton.click();
+
+    const event = await listener;
+    expect(event).to.exist;
+  });
+
+  it('sets count to `0` on clear ', async () => {
+    const clearButton = element.shadowRoot!.querySelector(
+      'typo3-button'
+    ) as HTMLButtonElement;
+    clearButton.click();
+
+    await elementUpdated(element);
+
+    expect(element.count).to.be.eq(0);
+  });
+});
