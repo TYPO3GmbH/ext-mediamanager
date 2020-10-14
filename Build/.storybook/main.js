d18a60a7ca6a6a91c08e2777501e76cb2017f7bd
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-14 10:57:27 +0200

    refactor: move stories in packages subdirectory

--- Build/.storybook/main.js
+++ Build/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../**/stories/*.stories.{js,md,mdx}'],
+  stories: ['../packages/*/stories/*.stories.{js,md,mdx}'],
   addons: [
     'storybook-prebuilt/addon-knobs/register.js',
     'storybook-prebuilt/addon-docs/register.js',
