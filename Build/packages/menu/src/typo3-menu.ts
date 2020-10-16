721a9967844351794e0ab7688021c71f0613c938
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-16 13:20:42 +0200

    fix: close menu on window.body click

--- Build/packages/menu/src/typo3-menu.ts
+++ Build/packages/menu/src/typo3-menu.ts
@@ -2,4 +2,27 @@ import { MenuBase } from '@material/mwc-menu/mwc-menu-base';
 import { customElement } from 'lit-element';
 
 @customElement('typo3-menu')
-export class Typo3Menu extends MenuBase {}
+export class Typo3Menu extends MenuBase {
+  protected onOpened() {
+    super.onOpened();
+    window.addEventListener('click', this.onBodyClick, { passive: true });
+  }
+
+  protected onClosed() {
+    super.onClosed();
+    window.removeEventListener('click', this.onBodyClick);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('click', this.onBodyClick);
+  }
+
+  onBodyClick = (event: MouseEvent): void => {
+    const path = event.composedPath();
+    if (path.indexOf(this) != -1) {
+      return;
+    }
+    this.close();
+  };
+}
