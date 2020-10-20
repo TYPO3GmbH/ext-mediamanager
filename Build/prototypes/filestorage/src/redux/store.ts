ebd5c63a8ab255d1087aea9f27141b6b4ff76c57
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-20 21:12:20 +0200

    fix: bypass tsconfig for REDUX_DEVTOOLS

--- Build/prototypes/filestorage/src/redux/store.ts
+++ Build/prototypes/filestorage/src/redux/store.ts
@@ -1,10 +1,10 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { rootReducer } from './ducks';
 
 export const store = createStore(
   rootReducer,
   process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    : null
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    : compose
 );
