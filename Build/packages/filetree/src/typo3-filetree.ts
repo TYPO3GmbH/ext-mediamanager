9db0dd4e1d39bce2658ed31a63a5aa4704136cb3
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-26 09:06:04 +0100

    fix: enforce exclusive selection for typo3-filetree

--- Build/packages/filetree/src/typo3-filetree.ts
+++ Build/packages/filetree/src/typo3-filetree.ts
@@ -1,6 +1,7 @@
 import { customElement } from 'lit-element';
 
 import { Typo3SvgTree } from './typo3-svg-tree';
+import { Typo3Node } from './lib/typo3-node';
 
 @customElement('typo3-filetree')
 export class Typo3Filetree extends Typo3SvgTree {
@@ -25,5 +26,13 @@ export class Typo3Filetree extends Typo3SvgTree {
       readableRootline: '',
       isMountPoint: false,
     };
+  }
+
+  _selectNode(node: Typo3Node) {
+    this._getSelectedNodes()
+      .filter(node => node.checked)
+      .forEach(node => (node.checked = false));
+
+    super._selectNode(node);
   }
 }
