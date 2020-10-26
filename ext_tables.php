7e52803bf87ada6356075bb4e775352279f30b89
Tobias Wojtylak <twojtylak@dfau.de>
2020-10-26 19:47:58 +0100

    feat: add basic module

---
+++ ext_tables.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'CmsFilelistNg',
+    'file',
+    'list_ng',
+    'bottom',
+    [
+        \TYPO3\CMS\FilelistNg\Backend\Controller\FilelistController::class => 'index',
+    ],
+    [
+        'access' => 'user,group',
+        'workspaces' => 'online,custom',
+        'icon' => 'EXT:cms_filelist_ng/Resources/Public/Icons/module-filelist-ng.svg',
+        'labels' => 'LLL:EXT:cms_filelist_ng/Resources/Private/Language/locallang_mod_file_list_ng.xlf',
+        'navigationComponentId' => null,
+        'inheritNavigationComponentFromMainModule' => false,
+    ]
+);
