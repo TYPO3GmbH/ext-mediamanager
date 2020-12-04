1d03840eae0deeeffc63e8ef65eb343c0ddce9e2
Tobias Wojtylak <twojtylak@dfau.de>
2020-12-04 18:09:20 +0100

    test: remove obsolete it_will_return_an_empty_array_if_folder_is_empty unit test (covered by functional tests)

--- Tests/Unit/Backend/Service/FileReferencesProviderTest.php
+++ Tests/Unit/Backend/Service/FileReferencesProviderTest.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
 namespace TYPO3\CMS\FilelistNg\Tests\Unit\Backend\Service;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Resource\Folder;
 use TYPO3\CMS\FilelistNg\Backend\Service\FileReferencesProvider;
 use TYPO3\CMS\FilelistNg\Backend\Service\FileReferencesProviderInterface;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -41,17 +40,5 @@ class FileReferencesProviderTest extends UnitTestCase
     public function it_implements_FileReferencesProviderInterface(): void
     {
         $this->assertInstanceOf(FileReferencesProviderInterface::class, $this->subject);
-    }
-
-    /**
-     * @test
-     */
-    public function it_will_return_an_empty_array_if_folder_is_empty(): void
-    {
-        $folder = $this->createMock(Folder::class);
-        $folder->method('getFiles')
-            ->willReturn([]);
-
-        $this->assertEquals([], $this->subject->getReferencesCount($folder));
     }
 }
